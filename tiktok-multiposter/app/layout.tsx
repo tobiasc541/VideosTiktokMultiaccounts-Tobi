28fb13e93207494f58f84ee2282de7ae4a377597
tobiasc541 <tobiasc541@gmail.com>
2026-09-14 18:42:28 -0300

    Use one locale engine across the entire app

--- tiktok-multiposter/app/layout.tsx
+++ tiktok-multiposter/app/layout.tsx
@@ -3,9 +3,7 @@ import "./brand.css";
 import "./login/auth-login.css";
 import CreatorPromoEntry from "./components/CreatorPromoEntry";
 import LocaleSelector from "./components/LocaleSelector";
-import LocaleIntegrityGuard from "./components/LocaleIntegrityGuard";
-import LocaleDynamicFinalizer from "./components/LocaleDynamicFinalizer";
-import LocaleFeatureCoverage from "./components/LocaleFeatureCoverage";
+import LocaleRuntime from "./components/LocaleRuntime";
 
 const appIcon = "/vyral-mark.svg";
 
@@ -21,13 +19,11 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="es">
+    <html lang="es" dir="ltr">
       <body>
         <CreatorPromoEntry />
         <LocaleSelector />
-        <LocaleIntegrityGuard />
-        <LocaleDynamicFinalizer />
-        <LocaleFeatureCoverage />
+        <LocaleRuntime />
         {children}
         <div className="vyralPersistentSignature" aria-hidden="true"><span>Powered by</span><strong>Tobias Carrizo</strong></div>
       </body>
