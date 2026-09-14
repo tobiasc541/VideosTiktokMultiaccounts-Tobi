1d1545270f28747b079ef695cd1a6458bc13a55b
tobiasc541 <tobiasc541@gmail.com>
2026-09-13 22:56:01 -0300

    Route VYRAL admins to admin center

--- tiktok-multiposter/app/page.tsx
+++ tiktok-multiposter/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-import { isLoggedIn } from "../lib/auth";
+import { getAdminSession, isLoggedIn } from "../lib/auth";
 import { listAccounts } from "../lib/tiktok";
 import Dashboard from "./ui/Dashboard";
 
@@ -7,6 +7,7 @@ export const dynamic = "force-dynamic";
 
 export default async function Home() {
   if (!(await isLoggedIn())) redirect("/login");
+  if (await getAdminSession()) redirect("/admin");
   const accounts = await listAccounts();
   return <Dashboard initialAccounts={accounts} />;
 }
