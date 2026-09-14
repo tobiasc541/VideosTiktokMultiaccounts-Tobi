5f6029b3e8f18d1e8efb23483ea99ec254587b47
tobiasc541 <tobiasc541@gmail.com>
2026-09-14 18:24:09 -0300

    Finalize dynamic locale strings after global translation

--- tiktok-multiposter/app/layout.tsx
+++ tiktok-multiposter/app/layout.tsx
@@ -4,6 +4,7 @@ import "./login/auth-login.css";
 import CreatorPromoEntry from "./components/CreatorPromoEntry";
 import LocaleController from "./components/LocaleController";
 import LocaleIntegrityGuard from "./components/LocaleIntegrityGuard";
+import LocaleDynamicFinalizer from "./components/LocaleDynamicFinalizer";
 
 const appIcon = "/vyral-mark.svg";
 
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <CreatorPromoEntry />
         <LocaleController />
         <LocaleIntegrityGuard />
+        <LocaleDynamicFinalizer />
         {children}
         <div className="vyralPersistentSignature" aria-hidden="true"><span>Powered by</span><strong>Tobias Carrizo</strong></div>
       </body>
