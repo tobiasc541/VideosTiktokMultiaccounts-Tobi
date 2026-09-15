442ca63eb6d3b00e27ccecb7a2c01b5453bf6fb5
tobiasc541 <tobiasc541@gmail.com>
2026-09-15 04:35:55 -0300

    Show complete Growth AI suite on login landing

--- tiktok-multiposter/app/login/layout.tsx
+++ tiktok-multiposter/app/login/layout.tsx
@@ -1,6 +1,7 @@
+import LandingAiSuite from "./LandingAiSuite";
 import LandingExpansion from "./LandingExpansion";
 import "./landing-next.css";
 
 export default function LoginLayout({children}:{children:React.ReactNode}){
-  return <>{children}<LandingExpansion/></>;
+  return <>{children}<LandingAiSuite/><LandingExpansion/></>;
 }
